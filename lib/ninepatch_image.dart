c539a9345a9130cca0a8a826ade09c5586e5d912
kim7881 <rlawls7881@gmail.com>
2023-09-21 10:13:00 +0900

    ninepatch_image

--- lib/ninepatch_image.dart
+++ lib/ninepatch_image.dart
@@ -4,12 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:image_pixels/image_pixels.dart';
 
 class NinePatchImage extends StatelessWidget {
-  ///hide lines  in 9 patch image
   final bool hideLines;
-  ///image provider  for  example  use AssetImage("assets/orange.9.png")
   final ImageProvider imageProvider;
-  /// child widget for example  Text("Hi welcome  ")
   final Widget child;
+
   const NinePatchImage({
     Key? key,
     required this.imageProvider,
@@ -20,68 +18,75 @@ class NinePatchImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ImagePixels(
-        imageProvider: imageProvider,
-        builder: (context, img) {
-          var leftTop = -1;
-          var leftBottom = -1;
+      imageProvider: imageProvider,
+      builder: (context, img) {
+        var leftTop = -1;
+        var leftBottom = -1;
 
-          var rightTop = -1;
-          var rightBottom = -1;
+        var rightTop = -1;
+        var rightBottom = -1;
 
-          if (img.height != null) {
-            for (var i = 0; i < img.height!; i++) {
-              var color = img.pixelColorAt!(0, i);
-              if (color == Colors.black) {
-                if (leftTop == -1) {
-                  leftTop = i;
-                }
-                if (leftTop != -1) {
-                  leftBottom = i;
-                }
+        if (img.height != null) {
+          for (var i = 0; i < img.height!; i++) {
+            var color = img.pixelColorAt!(0, i);
+            if (color == Colors.black) {
+              if (leftTop == -1) {
+                leftTop = i;
+              }
+              if (leftTop != -1) {
+                leftBottom = i;
               }
             }
-
-            for (var i = 0; i < img.width!; i++) {
-              var color = img.pixelColorAt!(i, 0);
-              if (color == Colors.black) {
-                if (rightTop == -1) {
-                  rightTop = i;
-                }
-                if (rightTop != -1) {
-                  rightBottom = i;
-                }
-              }
-            }
-          } else {
-            leftTop = 0;
-            leftBottom = 0;
-            rightTop = 0;
-            rightBottom = 0;
           }
 
-          return ClipPath(
-              clipper: BlackLineClipper(hideLines: hideLines),
-              child: Container(
-                // width: 300,
-                // height: 100,
-                padding: EdgeInsets.fromLTRB(
-                    rightTop.toDouble(),
-                    leftTop.toDouble(),
-                    (img.width ?? 0) - rightBottom.toDouble(),
-                    (img.height ?? 0) - leftBottom.toDouble()),
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: imageProvider,
-                      // fit: BoxFit.fill,
-                      centerSlice: Rect.fromLTRB(
-                          rightTop.toDouble(),
-                          leftTop.toDouble(),
-                          rightBottom.toDouble(),
-                          leftBottom.toDouble()),
-                    )),
-                child: child,
-              ));
-        });
+          for (var i = 0; i < img.width!; i++) {
+            var color = img.pixelColorAt!(i, 0);
+            if (color == Colors.black) {
+              if (rightTop == -1) {
+                rightTop = i;
+              }
+              if (rightTop != -1) {
+                rightBottom = i;
+              }
+            }
+          }
+        } else {
+          leftTop = 0;
+          leftBottom = 0;
+          rightTop = 0;
+          rightBottom = 0;
+        }
+
+        double leftPadding = rightTop.toDouble();
+        double topPadding = leftTop.toDouble();
+        double rightPadding = (img.width ?? 0) - rightBottom.toDouble();
+        double bottomPadding = (img.height ?? 0) - leftBottom.toDouble();
+
+        return ClipPath(
+          clipper: BlackLineClipper(hideLines: hideLines),
+          child: Container(
+            padding: EdgeInsets.fromLTRB(
+              leftPadding < 0 ? 0 : leftPadding,
+              topPadding < 0 ? 0 : topPadding,
+              rightPadding < 0 ? 0 : rightPadding,
+              bottomPadding < 0 ? 0 : bottomPadding,
+            ),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: imageProvider,
+                centerSlice: Rect.fromLTRB(
+                  rightTop.toDouble(),
+                  leftTop.toDouble(),
+                  rightBottom.toDouble(),
+                  leftBottom.toDouble(),
+                ),
+              ),
+            ),
+            child: child,
+          ),
+        );
+      },
+    );
   }
 }
 
@@ -89,6 +94,7 @@ class BlackLineClipper extends CustomClipper<Path> {
   final bool hideLines;
 
   BlackLineClipper({required this.hideLines});
+
   @override
   Path getClip(Size size) {
     final path = Path();
